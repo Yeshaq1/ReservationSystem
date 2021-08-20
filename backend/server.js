7602aa7cf5f79255e1894c083843ed149461fcbb
Yousef <yeshaq@realtermenergy.com>
2021-08-20 14:45:00 -0400

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors';
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
 const app = express();
+
+//connecting the database
+connectDB();
 
 app.use(express.json());
 
